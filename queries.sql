8b16b2323e9d82cb17d58e4cd77671092f0224d9
Eddie Madrigal <edmadrigal@yahoo.com>
2020-04-15 15:11:21 -0700

    sql statements added

--- queries.sql
+++ queries.sql
@@ -2,8 +2,19 @@
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 
+select p.productname, c.categoryname
+from Product as p
+join Category as c on p.categoryId = c.id
+
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
 
+select o.id, s.companyname
+from `order` as o
+join Shipper as s on o.shipvia = s.id
+where orderDate < '2012-08-09'
+
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
+
+
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
